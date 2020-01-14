825dc783dd364949887346f54187cb91a1190eb0
Robert Collins <rpc219@gmail.com>
2020-01-14 00:15:15 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,33 +1,33 @@
 def using_push(array, element)
-  
+  array.push(element)
 end
 
 def using_unshift(array, element)
-
+  array.unshift(element)
 end
 
 def using_pop(array)
-
+  array.pop
 end
 
 def pop_with_args(array)
-
+  array.pop(2)
 end
 
 def using_shift(array)
-
+  array.shift
 end
 
 def shift_with_args(array)
-
+  array.shift(2)
 end
 
 def using_concat(array, other_array)
-
+  array.concat(other_array)
 end
 
 def using_insert(array, element)
-
+  
 end
 
 def using_uniq(array)
